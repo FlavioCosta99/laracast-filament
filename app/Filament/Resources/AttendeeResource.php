f901827a86d94dc4a8123518c6ebb6f04efda3df
Flávio <fadc.99@hotmail.com>
2024-04-01 13:10:31 +0100

    Implement filament plugins

--- app/Filament/Resources/AttendeeResource.php
+++ app/Filament/Resources/AttendeeResource.php
@@ -7,8 +7,10 @@ use App\Filament\Resources\AttendeeResource\RelationManagers;
 use App\Filament\Resources\AttendeeResource\Widgets\AttendeeChartWidget;
 use App\Filament\Resources\AttendeeResource\Widgets\AttendeesStatsWidget;
 use App\Models\Attendee;
+use Awcodes\Shout\Components\Shout;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -45,6 +47,11 @@ class AttendeeResource extends Resource
     {
         return $form
             ->schema([
+                Shout::make('warn-price')
+                    ->type('warning')
+                    ->columnSpanFull()
+                    ->visible(fn (Get $get) => $get('ticket_cost') > 500)
+                    ->content(fn (Get $get) => 'This is ' . $get('ticket_cost') - 500 . ' more than the average ticket price.'),
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
@@ -53,6 +60,7 @@ class AttendeeResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('ticket_cost')
+                    ->lazy()
                     ->required()
                     ->numeric(),
                 Forms\Components\Toggle::make('is_paid')
